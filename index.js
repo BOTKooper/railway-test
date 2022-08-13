16cd7b7aa4b893ff61f3a594f8030c55bed97d3c
Alex Ozerov <botkooper@gmail.com>
2022-08-13 16:19:19 +0300

    upd

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify({ message: 'Hello World'}));
 });
 
-server.listen(3000);
+const port = +(process.env.PORT || 3000);
+server.listen(port, () => {
+    console.log('Server running on port ' + port);
+});
